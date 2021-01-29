36a738bc6c783f7b3a496cc74bbe0d0a28180a6a
Hasan Siam <hasansiam4545@gmail.com>
2021-01-30 01:54:21 +0600

    complete project

---
+++ main.js
@@ -0,0 +1,121 @@
+// initially setting the quantity of ticket to zero
+document.getElementById('firstClassTicketQuantity').value = 0;
+document.getElementById('economyClassTicketQuantity').value = 0;
+
+
+// event handler for first class ticket
+document.getElementById('firstClassPlusAdd').addEventListener('click', function () {
+
+    plusButtonHandler('firstClass', true);
+    ticketPrice('firstClass');
+    calculateTotalPrice();
+})
+
+document.getElementById('firstClassMinusAdd').addEventListener('click', function () {
+
+    plusButtonHandler('firstClass', false);
+    ticketPrice('firstClass');
+    calculateTotalPrice();
+})
+
+// event handler for economy class ticket
+
+document.getElementById('economyClassPlusAdd').addEventListener('click', function () {
+    plusButtonHandler('economyClass', true);
+    ticketPrice('economyClass');
+    calculateTotalPrice();
+})
+
+document.getElementById('economyClassMinusAdd').addEventListener('click', function () {
+    plusButtonHandler('economyClass', false);
+    ticketPrice('economyClass');
+    calculateTotalPrice();
+})
+
+
+
+// function for ticket quantity
+function plusButtonHandler(myclass, isIncrease) {
+    const myclassTicket = document.getElementById(myclass + 'TicketQuantity');
+    const myclassTicketQuantity = myclassTicket.value;
+    let myclassTicketAmount = parseInt(myclassTicketQuantity);
+    if (isIncrease == true) {
+        myclassTicketAmount = myclassTicketAmount + 1;
+    }
+    if (isIncrease == false && myclassTicketAmount > 0) {
+        myclassTicketAmount = myclassTicketAmount - 1;
+    }
+
+    myclassTicket.value = myclassTicketAmount;
+}
+
+
+
+// function for ticket price
+function ticketPrice(myClass) {
+    const myClassInput = document.getElementById(myClass + 'TicketQuantity').value;
+    const myClassQuantity = parseInt(myClassInput);
+    let myClassPrice = 0;
+    if (myClass == 'firstClass') {
+        myClassPrice = myClassQuantity * 150;
+    }
+    if (myClass == 'economyClass') {
+        myClassPrice = myClassQuantity * 100;
+    }
+    document.getElementById(myClass + 'TicketPrice').innerText = myClassPrice;
+}
+
+
+
+// function for subtotal,tax and total
+function calculateTotalPrice() {
+    const firstClassTicketQuantity = document.getElementById('firstClassTicketQuantity').value;
+    const firstClassTicket = parseInt(firstClassTicketQuantity);
+    const economyClassTicketQuantity = document.getElementById('economyClassTicketQuantity').value;
+    const economyClassTicket = parseInt(economyClassTicketQuantity);
+    let firstClassTicketPrice = 0;
+    let economyClassTicketPrice = 0;
+
+    if (firstClassTicket > 0) {
+        firstClassTicketPrice = firstClassTicket * 150;
+    }
+    if (economyClassTicket > 0) {
+        economyClassTicketPrice = economyClassTicket * 100;
+    }
+
+    const subtotal = firstClassTicketPrice + economyClassTicketPrice;
+    const tax = (subtotal * 10) / 100;
+    const total = subtotal + tax;
+    document.getElementById('subtotal').innerText = subtotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = total;
+}
+
+
+// function for onchange event handler
+document.getElementById('firstClassTicketQuantity').addEventListener('change', calculateTotalPrice);
+document.getElementById('economyClassTicketQuantity').addEventListener('change', calculateTotalPrice);
+
+
+
+// book now button click event handler
+document.getElementById('bookNow').addEventListener('click', function () {
+    document.getElementById('booking').style.display = 'none';
+    document.getElementById('confirm').style.display = 'block';
+    const firstClassTicket = document.getElementById('firstClassTicketQuantity').value;
+    const firstClassTicketAmount = parseInt(firstClassTicket);
+
+    const economyClassTicket = document.getElementById('economyClassTicketQuantity').value;
+    const economyClassTicketAmount = parseInt(economyClassTicket);
+
+    if (firstClassTicketAmount > 0 && economyClassTicketAmount < 1) {
+        document.getElementById('confirm').innerText = "congratulations! you have purchased " + firstClassTicketAmount + " first class ticket."
+    } else if (economyClassTicketAmount > 0 && firstClassTicketAmount < 1) {
+        document.getElementById('confirm').innerText = "congratulations! you have purchased " + economyClassTicketAmount + " economy class ticket."
+    } else if (firstClassTicketAmount > 0 && economyClassTicketAmount > 0) {
+        document.getElementById('confirm').innerText = "congratulations! you have purchased " + firstClassTicketAmount + " first class ticket and " + economyClassTicketAmount + " economy class ticket."
+    } else {
+        document.getElementById('confirm').innerText = "Please, first book any ticket!"
+    }
+
+})
